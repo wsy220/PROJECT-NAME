5ac2f6c58926af932e2b6b9fccbabc3f5c401a57
wsy <35551400@qq.com>
2018-11-05 15:59:55 +0800

    gggg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 
 import {HelloComponent} from 'component/hello.component';
+import { FlexDemoComponent } from './flex-demo/flex-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HelloComponent
+    AppComponent, HelloComponent, FlexDemoComponent
   ],
   imports: [
     BrowserModule,
